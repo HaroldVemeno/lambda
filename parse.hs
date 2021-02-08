7af38469867e89179ac4aef6ad6ef4f1cb0997f3
DJMiky007 <haroldvemeno@email.cz>
2021-02-08 19:47:28 +0100

    bugfix + test move

--- parse.hs
+++ parse.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE FlexibleContexts #-}
 
-module Parse (parseExpr, parseStmt) where
+module Parse (parseExpr, parseStmt, parseInput) where
 
 import Control.Applicative (liftA2)
 import Data.Bifunctor
@@ -9,6 +9,8 @@ import Text.Parsec
 import Text.Parsec.String
 import Types
 
+
+parseStmt :: SourceName -> String -> Result Statement
 parseStmt name str = case parse start name str of
   Right t -> Right t
   Left pe -> Left $ ParseError pe
@@ -31,7 +33,7 @@ startCommand :: Parser Command
 startCommand = ws *> command <* ws <* eof
 
 expr :: Parser Expr
-expr = abstr <|> try appl <|> parened <|> try name <|> var
+expr = try appl <|> abstr <|> parened <|> try name <|> var
 
 -- abstr = f <$ char '\\' <* ws <*> sepBy1 lower ws <* ws <* char '.' <* ws <*> pExpr
 abstr :: Parser Expr
@@ -52,14 +54,8 @@ var :: Parser Expr
 var = Var <$> lower
 
 appl :: Parser Expr
-appl =
-  try (Appl <$> applExpr <* ws <*> parened)
-    <|> f <$> applExpr <* ws <*> expr
+appl = foldl1 Appl <$> many1 applExpr
   where
-    f :: Expr -> Expr -> Expr
-    f a (Appl b c) = Appl (f a b) c
-    f a b = Appl a b
-
     applExpr :: Parser Expr
     applExpr = parened <|> abstr <|> try name <|> var
 
@@ -80,150 +76,3 @@ command =
   try (CommandLet <$ string "let" <* ws <*> nameStr <* ws <*> expr)
     <|> (CommandQuit <$ string "quit")
     <|> (CommandLoad <$ string "load" <*> manyTill anyChar eof)
-
-{-
->>> testParse
-Right True
-
--}
-
-testParse :: Result Bool
-testParse =
-  doTest
-    "Unbound varialble test"
-    "a"
-    (Var 'a')
-    &&& doTest
-      "Application test"
-      "bc"
-      (Appl (Var 'b') (Var 'c'))
-    &&& doTest
-      "Abstraction test"
-      "\\p.p"
-      (Abstr 'p' (Var 'p'))
-    &&& doTest
-      "Abstraction test 2"
-      "\\pqr.p"
-      (Abstr 'p' (Abstr 'q' (Abstr 'r' (Var 'p'))))
-    &&& doTest
-      "Parenthesis test"
-      "((h)((p)))"
-      (Appl (Var 'h') (Var 'p'))
-    &&& doTest
-      "Unbound varialble whitespace test"
-      " a "
-      (Var 'a')
-    &&& doTest
-      "Application whitespace test"
-      " b c "
-      (Appl (Var 'b') (Var 'c'))
-    &&& doTest
-      "Abstrastion whitespace test"
-      " \\ p  . p "
-      (Abstr 'p' (Var 'p'))
-    &&& doTest
-      "Parenthesis whitespace test"
-      "( (  h ) (  ( p   ) )  ) "
-      (Appl (Var 'h') (Var 'p'))
-    &&& doTest
-      "mix test 1"
-      "(\\ab.b\\ca.dacb)\\po.gho"
-      ( Appl
-          ( Abstr
-              'a'
-              ( Abstr
-                  'b'
-                  ( Appl
-                      (Var 'b')
-                      ( Abstr
-                          'c'
-                          ( Abstr
-                              'a'
-                              ( Appl
-                                  ( Appl
-                                      ( Appl
-                                          (Var 'd')
-                                          (Var 'a')
-                                      )
-                                      (Var 'c')
-                                  )
-                                  (Var 'b')
-                              )
-                          )
-                      )
-                  )
-              )
-          )
-          ( Abstr
-              'p'
-              ( Abstr
-                  'o'
-                  ( Appl
-                      ( Appl
-                          (Var 'g')
-                          (Var 'h')
-                      )
-                      (Var 'o')
-                  )
-              )
-          )
-      )
-    &&& doTestC
-      "quit command test"
-      "quit"
-      CommandQuit
-    &&& doTestC
-      "let command test"
-      "let 5 \\fx.f(f(f(f(fx))))"
-      ( CommandLet
-          "5"
-          ( Abstr
-              'f'
-              ( Abstr
-                  'x'
-                  ( Appl
-                      (Var 'f')
-                      ( Appl
-                          (Var 'f')
-                          ( Appl
-                              (Var 'f')
-                              ( Appl
-                                  (Var 'f')
-                                  ( Appl
-                                      (Var 'f')
-                                      (Var 'x')
-                                  )
-                              )
-                          )
-                      )
-                  )
-              )
-          )
-      )
-    &&& doTestC 
-      "load command test"
-      "load C:\\stuff\\haskell\\lambda.txt"
-      (CommandLoad " C:\\stuff\\haskell\\lambda.txt")
-
-  where
-    doTest c str r = doTestS c str (Expr r)
-    doTestC c str r = doTestS c str (Command r)
-    doTestS c str r =
-      let par = parseStmt c str
-          same = (== r) <$> par
-          un (Right s) = s
-       in if same == Right False
-            then
-              Left $
-                TestError
-                  ( "Failed " ++ c
-                      ++ "\nInput: "
-                      ++ str
-                      ++ "\nOutput: "
-                      ++ show (un par)
-                      ++ "\nExpected: "
-                      ++ show r
-                  )
-            else same
-    infixl 2 &&&
-    (&&&) = liftA2 (&&)
