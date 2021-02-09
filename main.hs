c602ada7f6206a11dc14c130b93fdab07fe23c9f
DJMiky007 <haroldvemeno@email.cz>
2021-02-09 22:12:23 +0100

    added convinient functions

--- main.hs
+++ main.hs
@@ -1,14 +1,20 @@
 module Main where
 
-import Types
-import Parse 
+import Control.Monad
+import Parse
 import Print
 import Reduce
-
 import Test
+import Types
 
 main :: IO ()
 main = undefined
 
 test :: Result Bool
-test = testParse 
+test = testParse
+
+quickParse :: IO ()
+quickParse = printExpr . parseExpr "input" =<< getLine
+
+quickReduce :: IO ()
+quickReduce = printExpr . (basicReduce <=< parseExpr "input") =<< getLine
