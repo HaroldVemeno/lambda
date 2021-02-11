39a31efa4e62daa89ac525844e002cb0480e1820
DJMiky007 <haroldvemeno@email.cz>
2021-02-11 20:07:34 +0100

    IOd doStmt to and teplDo for stepReduce also main is repl

--- main.hs
+++ main.hs
@@ -5,11 +5,12 @@ import Data.Map
 import Parse
 import Print
 import Reduce
+import System.IO
 import Test
 import Types
 
 main :: IO ()
-main = undefined
+main = repl
 
 repl :: IO ()
 repl = repl' defaultContext
@@ -17,26 +18,29 @@ repl = repl' defaultContext
     repl' c =
       do
         putStr "\n\\> "
+        hFlush stdout
         i <- getLine
-        let (o, c') = replDo (i, c)
+        (o, c') <- replDo (i, c)
         putStrLn o
         if quit c' then return () else repl' c'
 
-    replDo :: (String, Context) -> (String, Context)
+    replDo :: (String, Context) -> IO (String, Context)
     replDo (i, c) = doStmt c $ parseStmt "repl" i
 
-    doStmt :: Context -> Result Statement -> (String, Context)
-    doStmt c (Left err) = (show err, c)
-    doStmt c (Right (Expr e)) = (,) (showExpr $ reduce c e) c
+    doStmt :: Context -> Result Statement -> IO (String, Context)
+    doStmt c (Left err) = return (show err, c)
+    doStmt c (Right (Expr e)) = return $ (,) (showExpr $ reduce c e) c
     doStmt c (Right (Command k)) = case k of
-      CommandQuit -> (,) "Quitting" c {quit = True}
+      CommandQuit -> return $ (,) "Quitting" c {quit = True}
       CommandLet n e ->
-        let r = reduce c e
-         in case r of
-              Left err -> (show err, c)
-              Right e' -> ("Bound", c {names = insert n e' (names c)})
-      CommandTree e -> (rawShowTree e, c)
+        return $
+          let r = reduce c e
+           in case r of
+                Left err -> (show err, c)
+                Right e' -> ("Bound", c {names = insert n e' (names c)})
+      CommandTree e -> return (rawShowTree e, c)
       CommandLoad f -> undefined
+      CommandStep e -> stepReduce c e >> return ("", c)
 
 test :: Result Bool
 test = testParse
