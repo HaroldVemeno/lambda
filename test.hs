71b09ec8a08aa378a8d7cce56d3a517f0fde5520
DJMiky007 <haroldvemeno@email.cz>
2021-02-15 00:06:18 +0100

    lifted some functions to global scope

--- test.hs
+++ test.hs
@@ -74,6 +74,11 @@ reduceTests =
     ( "Random 1",
       "(\\ab.b\\ca.dacb)\\po.gho",
       "\\b.b(\\ca.dacb)"
+    ),
+    (
+      "unbound checking test",
+      "(\\fx.f(fx))(\\fx.f(fx))",
+      "\\fx.f(f(f(fx)))"
     )
   ]
 
@@ -84,7 +89,7 @@ commandTests =
       CommandQuit
     ),
     ( "Let command test",
-      "Let 5 \\fx.f(f(f(f(fx))))",
+      "Let 5 \\fx.f(f(f\n(f(fx))))",
       CommandLet
         "5"
         (Abstr 'f' (Abstr 'x' (Appl (Var 'f') (Appl (Var 'f') (Appl (Var 'f') (Appl (Var 'f') (Appl (Var 'f') (Var 'x'))))))))
@@ -100,52 +105,61 @@ testParse =
   foldl1 (&&&) (uncurry3 doTestE <$> parseTests)
     &&& foldl1 (&&&) (uncurry3 doTestR <$> reduceTests)
     &&& foldl1 (&&&) (uncurry3 doTestC <$> commandTests)
-  where
-    testEq :: Statement -> Statement -> Bool
-    testEq (Command a) (Command b) = a == b
-    testEq (Expr a) (Expr b) = alphaEq a b
-    testEq _ _ = False
 
-    uncurry3 :: (a -> b -> c -> r) -> (a, b, c) -> r
-    uncurry3 f (a, b, c) = f a b c
+testEq :: Statement -> Statement -> Bool
+testEq (Command a) (Command b) = a == b
+testEq (Expr a) (Expr b) = alphaEq a b
+testEq _ _ = False
 
-    doTestE c str r = doTestS c str (Expr r)
-    doTestC c str r = doTestS c str (Command r)
-    doTestS c str r =
-      let par = parseStmt c str
-          same = testEq r <$> par
-          un (Right s) = s
-       in if same == Right False
-            then
-              Left $
-                TestError
-                  ( "Failed " ++ c
-                      ++ "\nInput: "
-                      ++ str
-                      ++ "\nOutput: "
-                      ++ show (un par)
-                      ++ "\nExpected: "
-                      ++ show r
-                  )
-            else same
+uncurry3 :: (a -> b -> c -> r) -> (a, b, c) -> r
+uncurry3 f (a, b, c) = f a b c
 
-    doTestR c f t=
-      let pf = parseExpr (c ++ " (from)") f
-          pt = parseExpr (c ++ " (to)") t
-          same = liftA2 alphaEq (reduce defaultContext =<< pf) pt
-          un (Right s) = s
-       in if same == Right False
-            then
-              Left $
-                TestError
-                  ( "Failed " ++ c
-                      ++ "\nInput: "
-                      ++ f
-                      ++ "\nOutput: "
-                      ++ showExpr pt
-                      ++ "\nExpected: "
-                      ++ t
-                  )
-            else same
-    infixl 2 &&&
-    (&&&) = liftA2 (&&)
+doTestE :: [Char] -> [Char] -> Expr -> Result Bool
+doTestE c str r = doTestS c str (Expr r)
+
+doTestC :: [Char] -> [Char] -> Command -> Result Bool
+doTestC c str r = doTestS c str (Command r)
+
+doTestS :: [Char] -> [Char] -> Statement -> Result Bool
+doTestS c str r =
+  let par = parseStmt c str
+      same = testEq r <$> par
+      un (Right s) = s
+   in if same == Right False
+        then
+          Left $
+            TestError
+              ( "Failed " ++ c
+                  ++ "\nInput: "
+                  ++ str
+                  ++ "\nOutput: "
+                  ++ show (un par)
+                  ++ "\nExpected: "
+                  ++ show r
+              )
+        else same
+
+doTestR :: [Char] -> [Char] -> [Char] -> Result Bool
+doTestR c f t =
+  let pf = parseExpr (c ++ " (from)") f
+      pt = parseExpr (c ++ " (to)") t
+      same = liftA2 alphaEq (reduce defaultContext =<< pf) pt
+      un (Right s) = s
+   in if same == Right False
+        then
+          Left $
+            TestError
+              ( "Failed " ++ c
+                  ++ "\nInput: "
+                  ++ f
+                  ++ "\nOutput: "
+                  ++ showExpr pt
+                  ++ "\nExpected: "
+                  ++ t
+              )
+        else same
+
+infixl 2 &&&
+
+(&&&) :: Result Bool -> Result Bool -> Result Bool
+(&&&) = liftA2 (&&)
