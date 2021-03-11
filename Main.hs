915c78f6de135706df9db451a866ca6cbd50e217
Miky <haroldvemeno@email.cz>
2021-03-11 09:51:34 +0100

    added help for Set

--- Main.hs
+++ Main.hs
@@ -71,7 +71,15 @@ repl = repl' defaultContext
               "maxSizeRel" -> try v $ \e -> (prp (maxSizeRel c) e, c {maxSizeRel = e})
               "tryEta" -> try v $ \e -> (prp (tryEta c) e, c {tryEta = e})
               "forceNames" -> try v $ \e -> (prp (forceNames c) e, c {forceNames = e})
-              ""
+              "" -> (unlines 
+                [
+                  "maxReductions",
+                  "reduceStepSize",
+                  "maxSizeAbs",
+                  "maxSizeRel",
+                  "tryEta",
+                  "forceNames"
+                ], c)
               _ -> ("Unknown setting", c)
       CommandSkify e -> return (rawShowExpr $ skify c e, c) 
 
